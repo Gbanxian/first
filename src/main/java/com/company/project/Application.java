6de6d557eed31bd42caba1bf1a14ac365ce52b26
Gbanxian <904958418@qq.com>
2020-04-08 17:26:00 +0800

    first

---
+++ src/main/java/com/company/project/Application.java
@@ -0,0 +1,13 @@
+package com.company.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// 项目的启动了类
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
+
